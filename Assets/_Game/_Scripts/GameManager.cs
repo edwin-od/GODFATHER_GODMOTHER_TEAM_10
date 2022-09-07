30f94a8d8fb872d56053faf2b18ebce99ceb7038
KorYme <92892240+KorYme@users.noreply.github.com>
2022-09-07 15:12:54 +0200

    [ADD] : Throw the sword !

--- Assets/_Game/_Scripts/GameManager.cs
+++ Assets/_Game/_Scripts/GameManager.cs
@@ -49,16 +49,56 @@ public class GameManager : MonoBehaviour
     [SerializeField] EnemyController currentPlayer;
     public EnemyController Player => currentPlayer;
     EnemyController[] currentEnemies;
-    bool isPlayerTransition = false;
+    public bool isPlayerTransition = false;
+
+    [SerializeField] private GameObject sword;
+    [SerializeField] private float swordSpeed;
+    private Vector3 launchDir;
+    private float launchLimit;
+
+    public void ChangePlayer(EnemyController newPlayer)
+    {
+        currentPlayer.tag = "Enemy";
+        currentPlayer = newPlayer;
+        currentPlayer.tag = "Player";
+        isPlayerTransition = false;
+        sword.transform.SetParent(currentPlayer.transform);
+        sword.transform.localPosition = new Vector3(0, 0.4f, 0.6f);
+        sword.transform.localEulerAngles = new Vector3(90, 0, 0);
+    }
+
+    void LaunchLimit()
+    {
+        isPlayerTransition = false;
+        sword.transform.SetParent(currentPlayer.transform);
+        sword.transform.localPosition = new Vector3(0, 0.4f, 0.6f);
+        sword.transform.localEulerAngles = new Vector3(90, 0, 0);
+        currentPlayer.tag = "Player";
+    }
 
     private void Update()
     {
         if (currentPlayer && !isPlayerTransition)
         {
             Vector3 dir = (Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal")).normalized * Time.deltaTime * playerSpeed;
+            if (Input.GetKeyDown(KeyCode.JoystickButton3))
+            {
+                isPlayerTransition = true;
+                launchDir = currentPlayer.transform.forward;
+                sword.transform.SetParent(null);
+                launchLimit = 0;
+            }
             currentPlayer.transform.position += dir;
             currentPlayer.transform.rotation = Quaternion.LookRotation(dir.magnitude == 0 ? currentPlayer.transform.forward : dir.normalized);
         }
-        
+        if (isPlayerTransition)
+        {
+            sword.transform.position += launchDir.normalized * swordSpeed * Time.deltaTime;
+            launchLimit += Time.deltaTime;
+            if (launchLimit >= 2)
+            {
+                LaunchLimit();
+            }
+        }
     }
 }
