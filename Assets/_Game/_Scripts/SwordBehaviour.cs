43714f09812a372f4041777077e811f49d78e6d1
KorYme <92892240+KorYme@users.noreply.github.com>
2022-09-07 23:00:45 +0200

    [ADD] : Beginning bounce feature

--- Assets/_Game/_Scripts/SwordBehaviour.cs
+++ Assets/_Game/_Scripts/SwordBehaviour.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class SwordBehaviour : MonoBehaviour
 {
+    [SerializeField] private LayerMask obstaclesLayers;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy") && GameManager.Instance.isPlayerTransition)
+        if (!GameManager.Instance.isPlayerTransition) return;
+        if (other.CompareTag("Enemy"))
         {
             GameManager.Instance.ChangePlayer(other.GetComponent<EnemyController>());
+        }
+        if (obstaclesLayers == (obstaclesLayers | (1 << other.gameObject.layer)))
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, Vector3.forward);
+            Physics.Raycast(ray, out hit);
+            Vector3 reflectVec = Vector3.Reflect(Vector3.forward, hit.normal);
+
         }
     }
 }
